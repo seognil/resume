aa999bad9cb56e015d08b3a5382551777b63408a
seognil LC <seognil@gmail.com>
2021-03-10 00:45:46 +0800

    build: remove unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  sourceType: 'unambiguous',
   plugins: [
-    '@babel/plugin-transform-runtime',
     ['babel-plugin-module-resolver', { root: './', alias: { '#': './src' } }],
   ],
   presets: [
-    ['@babel/preset-env', { modules: 'cjs', targets: { node: 10 } }],
+    '@babel/preset-env',
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
